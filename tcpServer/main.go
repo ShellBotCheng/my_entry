bdcf00a2e151e5c2cc1f1e2b5ad6716ff7d2230d
zhenyuan.cheng <zhenyuan.cheng@shopee.com>
2023-02-27 21:14:55 +0800

    add

--- tcpServer/main.go
+++ tcpServer/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"hash/crc32"
+	"myEntry/pkg/log"
+	"myEntry/tcpServer/common"
 	"strconv"
 )
 
 func main() {
 	//服务初始化
-	//err := common.InitServer()
-	//if err != nil {
-	//	log.Panic("Tcp Server Start Error : %s\n", err)
-	//}
+	err := common.InitServer()
+	if err != nil {
+		log.Panic("Tcp Server Start Error : %s\n", err)
+	}
 	table := getTableName("admin")
 	fmt.Printf("table : %s", table)
 }
